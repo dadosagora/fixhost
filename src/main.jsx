7dde05ce59e6f49e742a713f94217562180c40e4
dadosagora <dadosagora121@gmail.com>
2025-08-20 09:12:28 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './index.css'
-import { AuthProvider } from './contexts/AuthContext'
-import ProtectedRoute from './routes/ProtectedRoute'
-import Login from './pages/Login'
-import Dashboard from './pages/Dashboard'
-import Tickets from './pages/Tickets'
-import Rooms from './pages/Rooms'
-import Users from './pages/Users'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";   // 👈 importa o App.jsx
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <AuthProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-          <Route path="/tickets" element={<ProtectedRoute><Tickets /></ProtectedRoute>} />
-          <Route path="/rooms" element={<ProtectedRoute><Rooms /></ProtectedRoute>} />
-          <Route path="/users" element={<ProtectedRoute><Users /></ProtectedRoute>} />
-        </Routes>
-      </BrowserRouter>
-    </AuthProvider>
+    <App />
   </React.StrictMode>
-)
+);
